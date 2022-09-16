0e29abef2623bd67f2d14fbb4b8b2fd14511b538
Robert Cabrera <cabrera.rch@gmail.com>
2022-09-16 20:32:49 +0200

    [FEATURE] Create router to basic Home and New Expense pages

---
+++ src/components/App/App.tsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "../../pages/Home/Home";
+import NewExpense from "../../pages/Expenses/NewExpense";
+import "../../index.css";
+
+function App() {
+  return (
+    <main className="app">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new-expense" element={<NewExpense />} />
+      </Routes>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/pages/Expenses/NewExpense.tsx
@@ -0,0 +1,5 @@
+const NewExpense = () => {
+  return <p>New expense</p>;
+};
+
+export default NewExpense;
